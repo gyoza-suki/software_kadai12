ede2f25f09bbdffe50fc357349a3e60af48321bc
Rena Nagai <35719029@stn.nitech.ac.jp>
2025-06-16 05:29:34 +0900

    mainでコメント追加

--- 8-3.cpp
+++ 8-3.cpp
@@ -5,24 +5,33 @@
 #include <iostream>
 #include <functional>
 
-std::function<double(double)> Fn(int n, std::function<double(double)> f) {
-    if (n == 0) {
-        return [](double x) { return x; };
-    } else {
-        auto g = Fn(n-1, f);
-        return [=](double x) { return f(g(x)); };
+std::function<double(double)> Fn(int n, std::function<double(double)> f)
+{
+    if (n == 0)
+    {
+        return [](double x)
+        { return x; };
+    }
+    else
+    {
+        auto g = Fn(n - 1, f);
+        return [=](double x)
+        { return f(g(x)); };
     }
 }
-    
-int main() {
-    auto pow = [](double x) { return x * x; };   // 二乗する関数 pow
-    auto add = [](double x) { return x + 1; };   // 1加算する関数 add
-    auto mul = [](double x) { return x * 2; };   // 倍にする関数 mul
+
+int main()
+{
+    auto pow = [](double x)
+    { return x * x; }; // 二乗する関数 pow
+    auto add = [](double x)
+    { return x + 1; }; // 1加算する関数 add
+    auto mul = [](double x)
+    { return x * 2; }; // 倍にする関数 mul
 
     std::cout << "「3を二乗」を2回適用：" << Fn(2, pow)(3) << std::endl;
     std::cout << "「3に1加算」を6回適用：" << Fn(6, add)(3) << std::endl;
-    
 
+    std::cout << "main で修正" << std::endl;
     return 0;
 }
-
