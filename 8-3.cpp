9d22a18f23f6aa858b85dae9be6a53e9753bebda
Rena Nagai <35719029@stn.nitech.ac.jp>
2025-06-16 05:00:45 +0900

    関数適用回数 5 --> 6

--- 8-3.cpp
+++ 8-3.cpp
@@ -17,9 +17,11 @@ std::function<double(double)> Fn(int n, std::function<double(double)> f) {
 int main() {
     auto pow = [](double x) { return x * x; };   // 二乗する関数 pow
     auto add = [](double x) { return x + 1; };   // 1加算する関数 add
+    auto mul = [](double x) { return x * 2; };   // 倍にする関数 mul
 
     std::cout << "「3を二乗」を2回適用：" << Fn(2, pow)(3) << std::endl;
     std::cout << "「3に1加算」を6回適用：" << Fn(6, add)(3) << std::endl;
+    std::cout << "「3を倍にする」を2回適用：" << Fn(2, add)(3) << std::endl;
 
     return 0;
 }
